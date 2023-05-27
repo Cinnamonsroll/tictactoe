5f835e45ed7e0f8c5288ca57cae323c989f6be26
Cinnamonsroll <cinnamonsrowolls@gmail.com>
2023-05-27 19:46:07 -0400

    Working restart button

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import math
 from colors import Background, Black, White
 from board import full_board, has_won
 from minimax import best_move
+
 pygame.init()
+
 WIDTH = 500
 ROWS = 3
+
 screen = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("TicTacToe")
+
 X_IMAGE = pygame.transform.scale(pygame.image.load("Images/x.png"), (150, 150))
 O_IMAGE = pygame.transform.scale(pygame.image.load("Images/o.png"), (150, 150))
 END_FONT = pygame.font.SysFont('courier', 40)
+
 screen.fill(Background)
+
 turn = 1
 grid = [[0] * ROWS for _ in range(ROWS)]
 running = True
 end_game = True
+
 def draw_grid():
     gap = WIDTH // ROWS
     x = 0
@@ -23,21 +30,23 @@ def draw_grid():
         x = i * gap
         pygame.draw.line(screen, Black, (x, 0), (x, WIDTH), 3)
         pygame.draw.line(screen, Black, (0, x), (WIDTH, x), 3)
+
 def on_click(grid):
-    global turn, images
+    global turn
     m_x, m_y = pygame.mouse.get_pos()
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-             x = (WIDTH // ROWS // 2) * (2 * i + 1)
-             y = (WIDTH // ROWS // 2) * (2 * j + 1)
-             dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
-             if dis < WIDTH // ROWS // 2 and grid[i][j] == 0:
+            x = (WIDTH // ROWS // 2) * (2 * i + 1)
+            y = (WIDTH // ROWS // 2) * (2 * j + 1)
+            dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
+            if dis < WIDTH // ROWS // 2 and grid[i][j] == 0:
                 grid[i][j] = turn
                 turn = 2
                 if not has_won(grid) and not full_board(grid):
                     move = best_move(grid, turn)
                     grid[move["x"]][move["y"]] = turn
                     turn = 1
+
 def display_message(content):
     screen.fill(Background)
     end_text = END_FONT.render(content, 1, Black)
@@ -46,6 +55,13 @@ def display_message(content):
     pygame.draw.rect(screen, Black, [(WIDTH - button_text.get_width()) // 2, ((WIDTH - button_text.get_height()) // 2) + 50, button_text.get_width() + 20, button_text.get_height() + 10])
     screen.blit(button_text, (((WIDTH - button_text.get_width()) // 2) + 10, ((WIDTH - button_text.get_height()) // 2) + 50))
     pygame.display.update()
+
+def restart_game():
+    global turn, grid, end_game
+    turn = 1
+    grid = [[0] * ROWS for _ in range(ROWS)]
+    end_game = True
+
 def render():
     screen.fill(Background)
     if end_game:
@@ -62,19 +78,27 @@ def render():
         pygame.display.update()
 
 while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if not end_game:
+                m_x, m_y = pygame.mouse.get_pos()
+                button_text = END_FONT.render("Restart", 1, White)
+                button_rect = pygame.Rect(((WIDTH - button_text.get_width()) // 2) + 10, ((WIDTH - button_text.get_height()) // 2) + 50, button_text.get_width(), button_text.get_height())
+                if button_rect.collidepoint(m_x, m_y):
+                    restart_game()
+                else:
+                    continue
+            else:
                 on_click(grid)
-        render()
-        if has_won(grid) or full_board(grid):
-            if has_won(grid):
-                display_message(["X", "O"][turn] + " has won!")
-                end_game = False
-            elif full_board(grid):
-                display_message("Draw")
-                end_game = False
-                
+    render()
+    if has_won(grid) or full_board(grid):
+        if has_won(grid):
+            display_message(["X", "O"][turn] + " has won!")
+            end_game = False
+        elif full_board(grid):
+            display_message("Draw")
+            end_game = False
 
 pygame.quit()
--- minimax.py
+++ minimax.py
@@ -1,37 +1,28 @@
 import math
-from board import flatten
-
 def get_available_states(grid):
-    available_points = []
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 0:
-                available_points.append({
-                    "x": i,
-                    "y": j
-                })
-    return available_points
+    return [{"x": i, "y": j} for i in range(len(grid)) for j in range(len(grid[i])) if grid[i][j] == 0]
+
 
 def has_won(grid, player):
     all_possible_wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-    flattened_board = flatten(grid)
+    flattened_board = [cell for row in grid for cell in row]
     for w in all_possible_wins:
         if all(flattened_board[index - 1] == player for index in w):
             return True
     return False
+
+
 
 def minimax(grid, turn, depth, is_max, alpha, beta):
     if has_won(grid, 1):
         return -10
     elif has_won(grid, 2):
         return 10
-    elif len(get_available_states(grid)) == 0:
+    elif not get_available_states(grid):
         return 0
 
     best_score = -math.inf if is_max else math.inf
-    current_spots = get_available_states(grid)
-
-    for spot in current_spots:
+    for spot in get_available_states(grid):
         i, j = spot['x'], spot['y']
         grid[i][j] = 2 if is_max else 1
         score = minimax(grid, turn, depth + 1, not is_max, alpha, beta)
@@ -50,14 +41,14 @@ def minimax(grid, turn, depth, is_max, alpha, beta):
     return best_score
 
 def best_move(grid, turn):
-    best_score = -math.inf
+    best_score = float('-inf')
     best_moves = []
     available_spots = get_available_states(grid)
 
     for spot in available_spots:
         i, j = spot['x'], spot['y']
         grid[i][j] = 2
-        score = minimax([row[:] for row in grid], turn, 0, False, -math.inf, math.inf)
+        score = minimax([row[:] for row in grid], turn, 0, False, float('-inf'), float('inf'))
         grid[i][j] = 0
 
         if score > best_score:
